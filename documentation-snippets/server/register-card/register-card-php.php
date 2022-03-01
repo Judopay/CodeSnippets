0d52794b40b73c4132c7ad7fce159574bc06753c
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-01 11:33:01 +0000

    Update register-card-php.php

--- documentation-snippets/server/register-card/register-card-php.php
+++ documentation-snippets/server/register-card/register-card-php.php
@@ -1,7 +1,23 @@
-[judo invokeTransactionWithType:TransactionTypePayment
-                  configuration:configuration
-                     completion:^(JPResponse *response, NSError *error) {
+//Prepare the register card request
+$registerCard = $judopay->getModel('RegisterCard');    
+     $attributes = [
+        'cardNumber' => '4976000000003436',
+        'expiryDate' => '12/25',
+        'cv2' => '452',
+        'judoId' => '100915867',
+        'yourConsumerReference' => 'consumerRef',
+        'yourPaymentReference' => 'paymentRef'
+    ];    $registerCard->setAttributeValues($attributes);
 
-     // Handle response / error
+//Send the request to Judopay
+try {
+        $response = $registerCard->create();
+             // Handle successful payment        
+             $cardToken = $response['cardDetails']['cardToken'];    
+                 } catch (\Exception $e) {
+                 // Handle the errors
+               return;
+    }
 
-}];
+//Get the card token from the reponse
+$cardToken = $response['cardDetails']['cardToken'];
