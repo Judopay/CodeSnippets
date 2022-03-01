cc315360138688ca96383249fc8eb127a89311f6
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-01 11:28:47 +0000

    Update check-card-php.php

--- documentation-snippets/server/check-card/check-card-php.php
+++ documentation-snippets/server/check-card/check-card-php.php
@@ -1,7 +1,22 @@
-[judo invokeTransactionWithType:TransactionTypePayment
-                  configuration:configuration
-                     completion:^(JPResponse *response, NSError *error) {
+//Prepare the CheckCard request
+$checkCard = $judopay->getModel('CheckCard');    $attributes = [
+        'cardNumber' => '4976000000003436',
+        'expiryDate' => '12/25',
+        'cv2' => '452',
+        'judoId' => '100915867',
+        'yourConsumerReference' => 'consumerRef',
+        'yourPaymentReference' => 'paymentRef' 
+    ];    $checkCard->setAttributeValues($attributes);
 
-     // Handle response / error
+//Send the request to Judopay
+try {
+    $response = $checkCard->create();
+    // Handle successful payment    
+     $cardToken = $response['cardDetails']['cardToken'];
+     } catch (\Exception $e) {
+    // Handle the errors
+    return;
+}
 
-}];
+//Get the card token from the response
+$cardToken = $response['cardDetails']['cardToken']; 
