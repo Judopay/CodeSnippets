2997968dd532511b5eb8103a6c4b5f471d8d033b
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-01 11:31:49 +0000

    Update register-card-csharp.cs

--- documentation-snippets/server/register-card/register-card-csharp.cs
+++ documentation-snippets/server/register-card/register-card-csharp.cs
@@ -1,7 +1,26 @@
-[judo invokeTransactionWithType:TransactionTypePayment
-                  configuration:configuration
-                     completion:^(JPResponse *response, NSError *error) {
+//Create an instance of the RegisterCard Model
+var register = new RegisterCardModel {         
+    JudoId = "yourJudoId",         
+    YourConsumerReference = "yourUniqueReference",         
+    YourPaymentReference = "yourPaymentReference",         
+    CardNumber = "4906000780007612",         
+    ExpiryDate = "12/25",        
+    CV2 = "452",         
+        CardAddress = new CardAddressModel {             
+            PostCode = "postCode"        
+                    }         
+        ThreeDSecure = new ThreeDSecureTwoModel {         
+              AuthenticationSource = ThreeDSecureTwoAuthenticationSource.Browser,           
+            ChallengeRequestIndicator = ThreeDSecureTwoChallengeRequestIndicator.ChallengePreferred,
+            ScaExemption = ThreeDSecureTwoScaExemption.SecureCorporate   
+        }
+};
 
-     // Handle response / error
+//Send the register request to Judopay
+var result = await 
+client.RegisterCards.Create(register);
+if (!result.HasError)
 
-}];
+{
+    var receipt = result.Response as PaymentReceiptModel;
+}
