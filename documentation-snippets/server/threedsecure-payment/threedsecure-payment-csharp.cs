c6d14cc9c436f0577f7fabbecdfc6568628672ee
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-02 11:16:53 +0000

    Update threedsecure-payment-csharp.cs

--- documentation-snippets/server/threedsecure-payment/threedsecure-payment-csharp.cs
+++ documentation-snippets/server/threedsecure-payment/threedsecure-payment-csharp.cs
@@ -39,4 +39,32 @@ var result = await client.Payments.Create(paymentModel);
 };
     
     //Resume the transaction flow to Judopay
+    //Use the ReceiptId from the original response
     var resumeResult = await client.ThreeDs.Resume3DSecureTwo(result.Response.ReceiptId, resumeModel);
+    
+    //Check the response. If no additional transaction checks are required, you will receive the usual paymentReceipt response.
+    //If additional transaction checks are required, you will receive the completion response.   
+    //Example completion response has been returned requesting: Challenge completion is needed for 3D Secure 2
+    {
+  "Response": {
+    "ThreeDSecure": {
+      "challengeUrl": "https://example.com/challenge/brw/acs",
+      "version": "2.1.0",
+      "md": "ewogICJ0aHJlZURTU2VydmVyVHJhbnNJRCIgOiAiYjNjY2IxYWItZTk5"
+    },
+    "receiptId": "68869013641206075392",
+    "message": "Issuer ACS has responded with a Challenge URL",
+    "result": "Challenge completion is needed for 3D Secure 2"
+  },
+}
+    
+  //Create an instance of the CompleteThreeDSecureTwo Model
+    var completeModel = new CompleteThreeDSecureTwoModel()
+{
+    CV2 = "452"
+};
+    
+    //Complete the transaction flow to Judopay
+    var completeResult = await client.ThreeDs.Complete3DSecureTwo(result.Response.ReceiptId, completeModel);
+    
+    
