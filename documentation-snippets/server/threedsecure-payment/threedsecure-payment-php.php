adf47f26dd0a443ba87e0afdfb5dbaee1c8afd43
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-02 10:29:17 +0000

    Update threedsecure-payment-php.php

--- documentation-snippets/server/threedsecure-payment/threedsecure-payment-php.php
+++ documentation-snippets/server/threedsecure-payment/threedsecure-payment-php.php
@@ -20,3 +20,23 @@ try
     } catch (\Exception $e) {
         return;
     }
+
+//Check the response. Example challenge response has been returned requesting Additional device data is needed for 3D Secure 2
+//Once the additional device data has been collected resume the transaction flow to Judopay:
+$resumeRequest = $judopay3ds2->getModel('ResumeThreeDSecureTwo');
+        $attributes = [
+            'receiptId' => $response['receiptId'],
+            'cv2' => '452',
+            'methodCompletion' => 'no'
+        ];
+        $resumeRequest->setAttributeValues($attributes);       
+ $response = $resumeRequest->update();
+
+//Complete the transaction flow to Judopay
+ $completeRequest = $judopay3ds2->getModel('CompleteThreeDSecureTwo');
+        $attributes = [
+            'receiptId' => $response['receiptId'],
+            'cv2' => '452'
+        ];
+        $completeRequest->setAttributeValues($attributes);       
+ $response = $completeRequest->update();
