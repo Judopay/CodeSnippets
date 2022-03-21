636190e5d288055d168ffa59384955a948b01532
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-21 17:08:41 +0000

    Update save-card-csharp.cs

--- documentation-snippets/server/save-card/save-card-csharp.cs
+++ documentation-snippets/server/save-card/save-card-csharp.cs
@@ -1,10 +1,10 @@
 Create an instance of the SaveCardModel
 var saveCardRequest = new SaveCardModel()
    {
-      CardNumber = "4976000000003436",
+      CardNumber = "4976000000003465",
       ExpiryDate = "12/25",
-      JudoId = cybersourceJudoId,
-      YourConsumerReference = "ASC123",
+      JudoId = yourJudoId,
+      YourConsumerReference = "yourConsumerReference",
  };
  
  //Send the request to Judopay
