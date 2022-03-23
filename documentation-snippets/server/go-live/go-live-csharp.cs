9dadfde3f1dfd01234e7d2ad0b4d13874feefe12
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-23 16:12:26 +0000

    Update go-live-csharp.cs

--- documentation-snippets/server/go-live/go-live-csharp.cs
+++ documentation-snippets/server/go-live/go-live-csharp.cs
@@ -1,5 +1,5 @@
 //In:
-JudoPaymentsFactory.Create method 
-//change the environment from Sandbox to Live:
+JudoPaymentsFactory.Create 
+//method, change the environment from Sandbox to Live:
 
 var client = JudoPaymentsFactory.Create(JudoEnvironment.Live, "YOUR_API_TOKEN", "YOUR_API_SECRET");
