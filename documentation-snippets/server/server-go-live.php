50876f59c798934837fe2dc8a8402ceeae51ad6f
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-23 16:03:32 +0000

    Create server-go-live.php

---
+++ documentation-snippets/server/server-go-live.php
@@ -0,0 +1,10 @@
+$judopay = new \Judopay(    
+array(       
+    'apiToken' => 'your-token,       
+    'apiSecret' => 'your-secret',        
+    'judoId' => 'your-judo-id',    
+    
+    //Set to true on production, defaults to false which is the sandbox  
+      
+    'useProduction' => true    )
+);
