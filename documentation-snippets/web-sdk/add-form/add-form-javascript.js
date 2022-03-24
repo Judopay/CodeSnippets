2f31cdb9338f496047e3af17b43ec355b340126b
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-24 10:35:06 +0000

    Create add-form-javascript.js

---
+++ documentation-snippets/web-sdk/add-form/add-form-javascript.js
@@ -0,0 +1,18 @@
+<script>
+var configMinimum = {
+    iframe: {
+    language: "en",
+    errorFieldId: 'judopay-errors',
+    showCardTypeIcons: true,
+    cardTypeIconRight: "10px",
+    cardTypeIconTop: "-2px",
+    backgroundColor: "#FFFFFF",
+    layout: "compact",
+    enabledPaymentMethods: ['CARD'],
+    isCountryAndPostcodeVisible: false,
+    isCardHolderNameVisible: true,
+    errorsDisplay: "HIDE_UNDER_FIELDS",
+    disableUnderline: false,
+    }
+}
+</script>
