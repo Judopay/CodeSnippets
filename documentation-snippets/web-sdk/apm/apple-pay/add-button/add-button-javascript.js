b31ac94c80137a9d90fadbe1bfea8520006f08cd
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-24 10:53:36 +0000

    Create add-button-javascript.js

---
+++ documentation-snippets/web-sdk/apm/apple-pay/add-button/add-button-javascript.js
@@ -0,0 +1,13 @@
+<script>
+var button = judo.digitalWallets.getApplePayButton({
+height: 38, 
+width: 240, 
+language: 'EN', 
+style: 'black', 
+type: 'buy'});
+
+button.onclick = handleApplePayButtonClick
+
+document.getElementById('apple-pay-button-container').appendChild(button)
+
+</script>   
