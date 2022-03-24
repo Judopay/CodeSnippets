4912de4227fbbbc105d8fea6ad1efed6b9f856ab
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-24 11:04:04 +0000

    Create add-button-javascript.js

---
+++ documentation-snippets/web-sdk/apm/google-pay/add-button/add-button-javascript.js
@@ -0,0 +1,6 @@
+//To display the Google Pay Button:
+const button = judo.digitalWallets.getGooglePayButton()
+
+//Add the Google Pay Button to your page:
+const container = document.getElementById('button-container')
+  container.append(button)
