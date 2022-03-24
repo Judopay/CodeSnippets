e4500fe949c68a5f2b4946ff4c154e75f83d192b
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-24 10:59:44 +0000

    Create button-click.javascript.js

---
+++ documentation-snippets/web-sdk/apm/apple-pay/button-click/button-click.javascript.js
@@ -0,0 +1,17 @@
+<script>
+function handleApplePayButtonClick() {
+
+let configuration = {  
+amount: '{{amount}}',  
+currency: '{{currency}}',  
+yourConsumerReference: '{{yourConsumerReference}}',  
+yourPaymentReference: '{{yourPaymentReference}}’,  
+judoId: '{{judoId}}', //initiativeContext: 'web.judopay.com' // optional, default = window.location.hostname}
+
+judo.digitalWallets
+.invokePaymentWithApplePay({{paymentSession}}, configuration)
+.then(response => console.log(JSON.stringify(response)))
+.catch(error => console.error(error))
+
+} 
+</script>
