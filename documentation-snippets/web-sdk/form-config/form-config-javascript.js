b6c8b0aa45b2a4c8ef173f6a8263ecbbca38debd
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-28 15:27:21 +0100

    Update form-config-javascript.js

--- documentation-snippets/web-sdk/form-config/form-config-javascript.js
+++ documentation-snippets/web-sdk/form-config/form-config-javascript.js
@@ -1,12 +1,20 @@
 var exampleConfig = {
     iframe: {
-        language: 'en',
-        errorFieldId: 'judopay-errors-2',
+        language: "en",
+        errorFieldId: 'judopay-errors',
+        cardTypeIconRight: "-400px",
+        cardTypeIconTop: "-50x",
+        backgroundColor: "#EAECEF",
         showCardTypeIcons: true,
-        cardTypeIconRight: '10px',
-        cardTypeIconTop: '-2px',
-        backgroundColor: '#e5e5e5',
-        layout: "compact",
+        layout: "vertical",
+        enabledPaymentMethods: ['CARD'],
+        defaultCountryCode: 'US',
+        isCountryAndPostcodeVisible: false,
+        isCardHolderNameVisible: false,
+        disableUnderline: true,
+        errorsDisplay: "HIDE_UNDER_FIELDS",
+        disableUnderline: true,
+        shouldAutofocues: false,
         fonts: [
             {
                 family: 'Roboto',
@@ -14,7 +22,8 @@ var exampleConfig = {
                 display: 'auto',
                 style: 'normal',
                 unicodeRange:
-                'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
+                'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, 
+                 U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
                 weight: '400'
             }
         ],
