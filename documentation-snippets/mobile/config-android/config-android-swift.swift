58b446d67ed3619eb9a2ed372d93417714654866
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 14:11:11 +0100

    Create config-android-swift.swift
    Xamarin - configuration for Android

---
+++ documentation-snippets/mobile/config-android/config-android-swift.swift
@@ -0,0 +1,3 @@
+//Add the code snippet after the Xamarin.Forms.Forms.Init method call inside the OnCreate method of your Activity
+Window.SetSoftInputMode(SoftInput.AdjustResize);
+AndroidBugfix5497.assistActivity(this);
