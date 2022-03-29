29adf9eb85412d1b8252a97385d5de95fcb26012
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 16:05:36 +0100

    Create token-response-java.java

---
+++ documentation-snippets/mobile/token-response/token-response-java.java
@@ -0,0 +1,17 @@
+//React Native
+try {
+    const judo: Judo = new JudoPay('myToken', 'mySecret')
+    const configuration: JudoConfiguration = {
+        judoId: 'myJudoId',
+        amount: amount,
+        reference: reference
+    }
+    const response = await judo.performTokenTransaction(
+        JudoTransactionMode.Payment,
+        configuration,
+        'myCardToken',
+        )
+// Handle response
+    } catch (exception) {
+// Handle exception
+}
