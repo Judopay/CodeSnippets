cd940c20ce853019b122a9a31b1445504f2b33b7
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 16:26:41 +0100

    Create config-paymentflow-java.java

---
+++ documentation-snippets/mobile/config-paymentflow/config-paymentflow-java.java
@@ -0,0 +1,13 @@
+//React Native
+//Import
+// - JudoConfiguration
+// - JudoAmount
+//J - udoReference
+//from the judo-react-native package:
+
+import JudoPay, {
+      JudoConfiguration,
+      JudoAmount,
+      JudoReference
+    } 
+from 'judo-react-native'
