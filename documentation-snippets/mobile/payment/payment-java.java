4d9f3b1d9bc155db4edaa849c7b842ffd569a7a2
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 15:38:33 +0100

    Update payment-java.java

--- documentation-snippets/mobile/payment/payment-java.java
+++ documentation-snippets/mobile/payment/payment-java.java
@@ -4,3 +4,11 @@ async invokeTransaction(
     type: JudoTransactionType,
     configuration: JudoConfiguration
 ): Promise<JudoResponse>
+
+//Make a payment
+import { ..., JudoPay, JudoTransactionType, ... } from 'judo-react-native'
+    const judo = new JudoPay('token', 'secret')
+        judo.isSandboxed = true
+    judo.invokeTransaction(JudoTransactionType.Payment, configuration)
+        .then((response) => {/* Handle response */})
+        .catch((error) => {/* Handle error */})
