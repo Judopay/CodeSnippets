5cf4724762f0b9721db36df22f29e3b709e8af1d
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 16:02:07 +0100

    Update token-payment-java.java

--- documentation-snippets/mobile/token-payment/token-payment-java.java
+++ documentation-snippets/mobile/token-payment/token-payment-java.java
@@ -1,1 +1,9 @@
-.
+//React Native
+//The method signature is as follows:
+
+public async performTokenTransaction(
+mode: JudoTransactionMode,
+configuration: JudoConfiguration,
+token: string
+): Promise<JudoResponse>
+
