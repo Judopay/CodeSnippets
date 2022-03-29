0ae20830e0083a8a9e23eee2e470993463e4529f
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 16:09:11 +0100

    Create config-ui-java.java

---
+++ documentation-snippets/mobile/config-ui/config-ui-java.java
@@ -0,0 +1,13 @@
+//React Native
+//Ceate the JudoUIConfiguration object:
+const uiConfiguration: JudoUIConfiguration = {
+    isAVSEnabled: false,
+    isCV2Enabled: true,
+    isPaymentButtonAmountVisible: true,
+    shouldDisplayAmount: true,
+    theme: theme,
+}
+const configuration: JudoConfiguration = {
+...
+uiConfiguration: uiConfiguration,
+...
