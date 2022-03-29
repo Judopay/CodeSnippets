4ac24677915b2f195295b775319b123176cd7e38
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 15:36:55 +0100

    Update payment-java.java

--- documentation-snippets/mobile/payment/payment-java.java
+++ documentation-snippets/mobile/payment/payment-java.java
@@ -1,4 +1,5 @@
 //invoke a payment with React Native SDK
+//The method for making a transaction and configuring the flow:
 async invokeTransaction(
     type: JudoTransactionType,
     configuration: JudoConfiguration
