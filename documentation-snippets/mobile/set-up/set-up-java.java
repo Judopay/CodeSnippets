d09cfb897dd434058c50f4f9e170cbb8f55f4c5c
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-29 15:29:02 +0100

    Create set-up-java.java

---
+++ documentation-snippets/mobile/set-up/set-up-java.java
@@ -0,0 +1,15 @@
+//React Native Set up
+{
+  "name": "my-app",
+  "version": "1.0.0",
+  "scripts": {
+   ...
+  },
+  "dependencies": {
+   "judo-react-native": "^2.1.0",
+   ...
+       },
+  "devDependencies": {
+    ...
+  }
+}
