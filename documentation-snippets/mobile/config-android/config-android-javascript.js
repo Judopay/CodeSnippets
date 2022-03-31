51e0c0096a83931b28090047c4777c42f2a6cd2c
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 09:12:23 +0100

    Update and rename config-android-kotlin.kt to config-android-javascript.js

---
+++ documentation-snippets/mobile/config-android/config-android-javascript.js
@@ -0,0 +1,4 @@
+//After the Xamarin.Forms.Forms.Init method call inside the OnCreate method of your Activity
+//Add the code snippet:
+Window.SetSoftInputMode(SoftInput.AdjustResize);
+AndroidBugfix5497.assistActivity(this);
--- documentation-snippets/mobile/config-android/config-android-kotlin.kt
+++
@@ -1,3 +0,0 @@
-//Add the code snippet after the Xamarin.Forms.Forms.Init method call inside the OnCreate method of your Activity
-Window.SetSoftInputMode(SoftInput.AdjustResize);
-AndroidBugfix5497.assistActivity(this);
