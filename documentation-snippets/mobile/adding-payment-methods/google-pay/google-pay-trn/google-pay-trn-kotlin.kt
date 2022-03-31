be1bd92247c790523a85b0706a4303a08400a7d3
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:30:58 +0100

    Create google-pay-trn-kotlin.kt

---
+++ documentation-snippets/mobile/adding-payment-methods/google-pay/google-pay-trn/google-pay-trn-kotlin.kt
@@ -0,0 +1,17 @@
+//Android
+//Add the setGooglePayConfiguration method to the Judo builder
+
+//Set the PaymentWidgetType to: 
+//- GOOGLE_PAY or
+//- PRE_AUTH_GOOGLE_PAY
+
+//Start the Judo activity:
+
+val judo = Judo.Builder(PaymentWidgetType.GOOGLE_PAY)             
+    ...              
+    .setGooglePayConfiguration(googlePayConfiguration)              
+    .build()
+    
+val intent = Intent(this, JudoActivity::class.java);
+intent.putExtra(Judo.JUDO_OPTIONS, judo);
+startActivityForResult(intent, PAYMENT_REQUEST_CODE);
