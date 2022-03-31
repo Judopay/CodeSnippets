b4f120b57cf113c9a8ce1964c504da4c741b54ad
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 10:30:57 +0100

    Create apple-pay-trn-java.js

---
+++ documentation-snippets/mobile/adding-payment-methods/apple-pay/apple-pay-trn/apple-pay-trn-java.js
@@ -0,0 +1,24 @@
+//React Native
+//An example of an Apple Pay™ transaction:
+import { ..., JudoTransactionMode, ... } from 'judo-react-native'
+
+// Describing the purchased items
+const itemOne: JudoPaymentSummaryItem = {    
+	label: 'Apple iPhone XR',    
+	amount: '699.95',
+}
+const itemTwo: JudoPaymentSummaryItem = {    
+	label: 'Apple Leather Case',    
+	amount: '55.50',
+}
+const total: JudoPaymentSummaryItem = {    
+	label: 'Tim Apple',    
+	amount: '755.45',
+}
+// Describe the shipping method
+const delivery: JudoShippingMethod = {    
+	identifier: 'delivery-id',    
+	label: 'Delivery',    
+	detail: 'Deliver to your home address',    
+	amount: '15.0',
+}
