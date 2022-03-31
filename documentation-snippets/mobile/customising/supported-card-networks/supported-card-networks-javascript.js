9ddd3058e6fb4b755ca3b70093b0a972a78f47ad
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 09:30:07 +0100

    Update and rename supported-card-networks-java.java to supported-card-networks-javascript.js

---
+++ documentation-snippets/mobile/customising/supported-card-networks/supported-card-networks-javascript.js
@@ -0,0 +1,13 @@
+//React Native
+//An example of the JudoCardNetwork:
+enum JudoCardNetwork {
+    Visa = 1 << 0,
+    Mastercard = 1 << 1,
+    Maestro = 1 << 2,
+    Amex = 1 << 3,
+    ChinaUnionPay = 1 << 4,
+    JCB = 1 << 5,
+    Discover = 1 << 6,
+    DinersClub = 1 << 7,
+    All = 1 << 8,
+}
