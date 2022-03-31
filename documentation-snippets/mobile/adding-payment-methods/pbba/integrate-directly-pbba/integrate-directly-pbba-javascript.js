def24e5026312511ff59598972d00070b845b567
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:00:52 +0100

    Create integrate-directly-pbba-javascript.js

---
+++ documentation-snippets/mobile/adding-payment-methods/pbba/integrate-directly-pbba/integrate-directly-pbba-javascript.js
@@ -0,0 +1,11 @@
+//React Native
+//To integrate directly to your app:
+
+//Instead of calling invokePayByBankApp:
+//Add the branded PaybyBankApp button
+//Add the method as a button press action
+//Expose the PaybyBankApp button as follows:
+import { JudoPBBAButton } from 'judo-react-native'
+<TouchableOpacity onPress={this.invokePayByBankApp}>
+<JudoPBBAButton style={{ flex: 1 }} />
+</TouchableOpacity>
