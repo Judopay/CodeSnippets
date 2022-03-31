60969811f3461f0ec3493b0e19d811cbd0ab0589
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 09:24:55 +0100

    Create trn-amount-javascript.js

---
+++ documentation-snippets/mobile/customising/trn-amount-javascript.js
@@ -0,0 +1,15 @@
+//React Native
+//From the imported JudoAmount interface:
+
+interface JudoAmount {
+    value: string,
+    currency: string,
+}
+
+//Create a new variable that implements the JudoAmount interface
+//Add the variable to the configuration object:
+
+const amount: JudoAmount = {
+  value: '999.99',
+  currency: 'GBP'
+}
