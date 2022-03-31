a7c1f31b88a60ab6badaef4e9e68fbfd41e651ee
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 16:15:10 +0100

    Create configure-apple-pay-objc.c

---
+++ documentation-snippets/mobile/adding-payment-methods/apple-pay/configure-apple-pay/configure-apple-pay-objc.c
@@ -0,0 +1,11 @@
+//To set all the Apple Pay™ required parameters, use the JPApplePayConfiguration:
+
+NSDecimalNumber *itemOnePrice = [NSDecimalNumber decimalNumberWithString:@"649.99"];
+NSDecimalNumber *itemTwoPrice = [NSDecimalNumber decimalNumberWithString:@"55.00"];
+NSDecimalNumber *totalPrice = [NSDecimalNumber decimalNumberWithString:@"704.99"];
+
+NSArray *mySummaryItems = @[  
+    [JPPaymentSummaryItem itemWithLabel:@"iPhone XR 254GB" amount:itemOnePrice],  
+    [JPPaymentSummaryItem itemWithLabel:@"iPhone XR Case" amount:itemTwoPrice],  
+    [JPPaymentSummaryItem itemWithLabel:@"Apple Store" amount:totalPrice],
+];
