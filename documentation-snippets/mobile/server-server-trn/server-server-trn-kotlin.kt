4c63c1e758abe816b507cfb2e874556d4532a5cf
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:44:44 +0100

    Update server-server-trn-kotlin.kt

--- documentation-snippets/mobile/server-server-trn/server-server-trn-kotlin.kt
+++ documentation-snippets/mobile/server-server-trn/server-server-trn-kotlin.kt
@@ -1,1 +1,11 @@
-.
+//Android
+//On the Judo builder set the PaymentWidgetType to:
+
+val judo = Judo.Builder(PaymentWidgetType.SERVER_TO_SERVER_PAYMENT_METHODS)
+     .setApiToken("my-token")
+     .setApiSecret("my-secret")
+     .setIsSandboxed(true)
+     .setJudoId("my-judo-id")
+     .setAmount(amount)
+     .setReference(reference)
+            .build()
