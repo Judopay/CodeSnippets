e35d736caf1d2e28e6a67582ccbda072275b0cca
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 10:35:34 +0100

    Create display-google-pay-javascript.js

---
+++ documentation-snippets/mobile/adding-payment-methods/google-pay/configure-google-pay/display-google-pay/display-google-pay-javascript.js
@@ -0,0 +1,13 @@
+//React Native
+
+//Add the googlePayConfiguration property to the JudoConfiguration instance:
+const configuration: JudoConfiguration = {    
+    judoId: 'my-judo-id',    
+    amount: amount,    
+    reference: reference,    
+    googlePayConfiguration: googlePayConfiguration,
+}
+
+//Invoke the GooglePay method and handle the response:
+judo.invokeGooglePay(JudoTransactionMode.Payment, configuration)  
+.then((response) => {/* Handle response */})
