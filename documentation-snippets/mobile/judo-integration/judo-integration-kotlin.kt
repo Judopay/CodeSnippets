322fca978f52165a2be57e94c27a82061882cae4
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 14:03:42 +0100

    Create judo-integration-kotlin.kt

---
+++ documentation-snippets/mobile/judo-integration/judo-integration-kotlin.kt
@@ -0,0 +1,27 @@
+//Android
+//To add Judopay as a dependency to your current project:
+
+//Navigate to your build.gradle file
+
+//In the dependencies section add the latest 
+com.judopay:judokit-android:{version}
+
+//An example of how your build.gradle might look:
+android {
+    ...
+}
+
+dependencies {
+    ...
+    implementation 'com.judopay:judokit-android:2.1.1'
+    ...
+}
+ 
+
+Ensure the Maven Central Repository has been added:
+
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+}
