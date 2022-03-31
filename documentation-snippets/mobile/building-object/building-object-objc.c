84a39b5bd74d60a08b54d85e3b6290ffa6a4cf5d
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 14:13:31 +0100

    Create building-object-objc.c

---
+++ documentation-snippets/mobile/building-object/building-object-objc.c
@@ -0,0 +1,20 @@
+//iOS
+//To configure the JPConfiguration object add your:
+
+//- JudoId
+//- Amount
+//- ConsumerReference
+
+
+JPAmount *myAmount;
+JPReference *myReference;
+JPConfiguration *myConfiguration;
+
+    myAmount = [[JPAmount alloc] initWithAmount:@"0.01"
+                                   currency:@"GBP"];
+
+    myReference = [[JPReference alloc] initWithConsumerReference:@"my-reference"];
+
+    myConfiguration = [JPConfiguration alloc] initWithJudoID:@"my-judo-id"
+                      amount:myAmount
+                      reference:myReference];
