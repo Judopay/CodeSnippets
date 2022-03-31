5d0bb014037921a1bc68e7347c791209248b7511
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:03:55 +0100

    Create display-ideal-javascript.js

---
+++ documentation-snippets/mobile/adding-payment-methods/ideal/display-ideal/display-ideal-javascript.js
@@ -0,0 +1,10 @@
+//React Native
+//To add iDEAL support to your Payments Widget set:
+The currency code to EUR (Euro)The judoId
+
+//An example of a valid iDEAL configuration:
+const   configuration:  JudoConfiguration   = {             
+	...            
+	judoId: 'myJudoId'           
+  	...
+}
