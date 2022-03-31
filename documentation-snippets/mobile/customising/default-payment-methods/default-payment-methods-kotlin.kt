0b427b503cbd66ca5250d964288824bcfe8ae7b3
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 09:40:29 +0100

    Update default-payment-methods-kotlin.kt

--- documentation-snippets/mobile/customising/default-payment-methods/default-payment-methods-kotlin.kt
+++ documentation-snippets/mobile/customising/default-payment-methods/default-payment-methods-kotlin.kt
@@ -1,1 +1,15 @@
-.
+//Android
+//Call the setPaymentMethods on the Judo builder
+
+//Pass an array of PaymentMethod enum values:
+
+val paymentMethods = arrayOf(
+  PaymentMethod.CARD,
+  PaymentMethod.GOOGLE_PAY,
+  PaymentMethod.IDEAL
+)
+
+val judo = Judo.Builder(PaymentWidgetType.CARD_PAYMENT)
+            ...
+            .setPaymentMethods(paymentMethods)
+            .build()
