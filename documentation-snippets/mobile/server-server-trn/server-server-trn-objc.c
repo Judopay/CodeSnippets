0e9207fb8bad3893f5c150d931baa4d3b10baba5
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:47:13 +0100

    Update server-server-trn-objc.c

--- documentation-snippets/mobile/server-server-trn/server-server-trn-objc.c
+++ documentation-snippets/mobile/server-server-trn/server-server-trn-objc.c
@@ -1,1 +1,10 @@
-.
+//iOS
+//To specify a server-to-server transaction:
+
+//Set the TransactionMode to TransactionModeServerToServer when calling the Judopay methods:
+
+[self.judoKitSession invokePaymentMethodScreenWithMode:TransactionModeServerToServer
+                configuration:configuration
+                completion:^(JPResponse *response, NSError *error) {
+    // Handle the payment token returned from the response
+}];
