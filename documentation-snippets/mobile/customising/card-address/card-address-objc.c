be1b09bbde1a33b4163861b86e36060e14b6ad00
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:20:40 +0100

    Create card-address-objc.c

---
+++ documentation-snippets/mobile/customising/card-address/card-address-objc.c
@@ -0,0 +1,9 @@
+//iOS
+//Set the cardAddress parameter in the JPConfiguration object:
+
+JPAddress *cardAddress = [[JPCardAddress alloc] initWithLine1:@"address-1"
+                 line2:@"address-2"
+                 line3:nil
+                 town:@"example-town"
+                 countryCode:@"GB"
+                 postCode:@"EX4 MPL3"];
