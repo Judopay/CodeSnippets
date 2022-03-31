7304aa5f60c3a12d2646cb1f2c54554da8145012
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:11:14 +0100

    Create config-ui-objc.c

---
+++ documentation-snippets/mobile/config-ui/config-ui-objc.c
@@ -0,0 +1,12 @@
+//iOS
+//An example of the JPUIConfiguration object:
+
+JPUIConfiguration *uiConfig = [JPUIConfiguration new];
+    uiConfig.shouldPaymentMethodsDisplayAmount = NO;
+    uiConfig.shouldPaymentButtonDisplayAmount: = NO;
+    uiConfig.isAVSEnabled = YES;
+    uiConfig.shouldPaymentMethodsVerifySecurityCode = YES;
+
+configuration.uiConfiguration = uiConfig;
+
+//The JPUIConfiguration instance is set as part of the JPConfiguration object that is passed to our transactions
