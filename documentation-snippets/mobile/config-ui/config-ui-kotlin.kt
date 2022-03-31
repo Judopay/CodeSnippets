6fe6626395187207b32e925b535a6bfe3f91afd0
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:10:13 +0100

    Create config-ui-kotlin.kt

---
+++ documentation-snippets/mobile/config-ui/config-ui-kotlin.kt
@@ -0,0 +1,19 @@
+//Android
+//To create the UIConfiguration object:
+import com.judopay.judokit.android.UIConfiguration
+
+val uiConfiguration = UIConfiguration.Builder()
+           .setAvsEnabled(true)
+           .setShouldPaymentMethodsDisplayAmount(true)
+           .setShouldPaymentMethodsVerifySecurityCode(true)
+           .setShouldPaymentButtonDisplayAmount(true)
+           .build()
+
+//Call setUIConfiguration on the Judo builder
+
+//Pass the uiConfigurationobject:
+
+val judo = Judo.Builder(PaymentWidgetType.PAYMENT_METHODS)
+    ...
+        .setUIConfiguration(uiConfiguration)
+        .build()
