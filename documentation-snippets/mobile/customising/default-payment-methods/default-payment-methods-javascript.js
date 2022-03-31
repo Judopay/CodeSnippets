98a1270ff11b97f99bcc3b440667dc6fb8dda5e0
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 09:39:34 +0100

    Update and rename default-payment-methods-java.java to default-payment-methods-javascript.js

---
+++ documentation-snippets/mobile/customising/default-payment-methods/default-payment-methods-javascript.js
@@ -0,0 +1,11 @@
+//React Native
+//Change the paymentMethods property to the JudoPaymentMethod value in the JudoConfiguration instance:
+
+//An example of the JudoPaymentMethod:
+enum JudoPaymentMethod {
+    Card = 1 << 0,
+    ApplePay = 1 << 1,
+    GooglePay = 1 << 2,
+    iDEAL = 1 << 3,
+    All = 1 << 4,
+}
