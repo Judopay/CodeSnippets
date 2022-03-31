f31b746d3616fd7e0a0bb518cc5c8a25dbfe82d5
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 10:33:55 +0100

    Update configure-google-pay-javascript.js

--- documentation-snippets/mobile/adding-payment-methods/google-pay/configure-google-pay/configure-google-pay-javascript.js
+++ documentation-snippets/mobile/adding-payment-methods/google-pay/configure-google-pay/configure-google-pay-javascript.js
@@ -1,1 +1,30 @@
-.
+//React Native
+//Provide a JudoGooglePayConfiguration property.
+
+//Example of a googlePayConfiguration property:
+import {     
+JudoGooglePayConfiguration,     
+JudoGooglePayEnvironment,     
+JudoAddressFormat,     
+JudoBillingAddressParameters,     
+JudoShippingAddressParameters 
+} from 'judo-react-native'  
+
+const billingParameters: JudoBillingAddressParameters = {     
+addressFormat: JudoAddressFormat.MINIMAL,     
+isPhoneNumberRequired: true, 
+}  
+
+const shippingParameters: JudoShippingAddressParameters = {    
+allowedCountryCodes: ["GB", "US"],     
+isPhoneNumberRequired: false, }  export 
+
+const googlePayConfiguration: JudoGooglePayConfiguration = {     
+countryCode: 'GB',     
+environment: JudoGooglePayEnvironment.TEST,     
+isEmailRequired: true,     
+isBillingAddressRequired: true,     
+billingAddressParameters: billingParameters,     
+isShippingAddressRequired: true,     
+ShippingAddressParameters: shippingParameters 
+}
