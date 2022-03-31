34329fd3f68610418dc11f0e066b6b1f78bd02c9
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:16:53 +0100

    Update supported-card-networks-objc.c

--- documentation-snippets/mobile/customising/supported-card-networks/supported-card-networks-objc.c
+++ documentation-snippets/mobile/customising/supported-card-networks/supported-card-networks-objc.c
@@ -1,1 +1,4 @@
-.
+//iOS
+//Set the supportedCardNetworks property of the JPConfiguration object 
+//For the purpose of this exercise, Visa and MasterCard are set as the supported card networks: 
+configuration.supportedCardNetworks = CardNetworkVisa | CardNetworkMasterCard;
