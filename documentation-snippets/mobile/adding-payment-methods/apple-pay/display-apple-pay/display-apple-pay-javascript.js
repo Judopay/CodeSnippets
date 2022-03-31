20afb7268de272b621be557e8e1e508d6f8391a2
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 10:28:40 +0100

    Create display-apple-pay-javascript.js

---
+++ documentation-snippets/mobile/adding-payment-methods/apple-pay/display-apple-pay/display-apple-pay-javascript.js
@@ -0,0 +1,16 @@
+//React Native
+//To display Apple Pay™ in the Payments Widget
+
+//Add the applePayConfiguration property to the JudoConfiguration instance:
+const configuration: JudoConfiguration = { 
+    judoId: 'my-judo-id',    
+    amount: amount,    
+    reference: reference,    
+    applePayConfiguration: applePayConfiguration,
+}
+
+//Call the ApplePay method and handle the response:
+import { ..., JudoTransactionMode, ... } from 'judo-react-native'
+judo.invokeApplePay(JudoTransactionMode.Payment, configuration)
+.then((response) => {/* Handle response */})
+.catch((error) => {/* Handle error */})
