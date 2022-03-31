041678f39c13f6c886abaf929e33f62619d2a942
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 10:16:28 +0100

    Update payments-widget-kotlin.kt

--- documentation-snippets/mobile/customising/payments-widget/payments-widget-kotlin.kt
+++ documentation-snippets/mobile/customising/payments-widget/payments-widget-kotlin.kt
@@ -1,1 +1,22 @@
-.
+//To set up the Payments Widget for Android
+//Change the PaymentWidgetType to either:
+
+//PAYMENT_METHODS or, 
+
+//PRE_AUTH_PAYMENT_METHODS
+
+//For example:
+val judo = Judo.Builder(PaymentWidgetType.PAYMENT_METHODS)
+            .setApiToken("my-token")
+            .setApiSecret("my-secret")
+            .setIsSandboxed(true)
+            .setJudoId("my-judo-id")
+            .setAmount(amount)
+            .setReference(reference)
+            .setGooglePayConfiguration(googlePayConfiguration)
+            .build()
+            
+//Start the Judo Activity:
+val intent = Intent(this, JudoActivity::class.java);
+intent.putExtra(Judo.JUDO_OPTIONS, judo);
+startActivityForResult(intent, PAYMENT_REQUEST_CODE);
