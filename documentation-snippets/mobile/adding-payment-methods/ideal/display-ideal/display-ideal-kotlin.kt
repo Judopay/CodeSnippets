a90557d1579ded4ca3e32bdcf6197d9473ede474
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:33:13 +0100

    Create display-ideal-kotlin.kt

---
+++ documentation-snippets/mobile/adding-payment-methods/ideal/display-ideal/display-ideal-kotlin.kt
@@ -0,0 +1,13 @@
+//Android
+//To add iDEAL support to your Payments Widget, set:
+//- The currency code to EUR (Euro)
+//- The JudoId
+
+//The judoId parameter can be set by calling setJudoId on the Judo builder.
+
+//An example of a valid iDEAL configuration:
+val judo = Judo.Builder(PaymentWidgetType.PAYMENT_METHODS)         
+       ...            
+    .setJudoId("my-judo-id")           
+    .setCurrency("EUR")           
+    .build()
