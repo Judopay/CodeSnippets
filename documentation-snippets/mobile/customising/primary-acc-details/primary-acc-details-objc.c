80a4a7c606fffb016cdf13eb0e746aad6462e252
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:12:57 +0100

    Update primary-acc-details-objc.c

--- documentation-snippets/mobile/customising/primary-acc-details/primary-acc-details-objc.c
+++ documentation-snippets/mobile/customising/primary-acc-details/primary-acc-details-objc.c
@@ -1,1 +1,12 @@
-.
+//iOS
+//Set the primaryAccountDetails property of the JPConfiguration object:
+
+JPPrimaryAccountDetails *accountDetails;
+accountDetails = [JPPrimaryAccountDetails new];
+
+    accountDetails.name = @"example-name";
+    accountDetails.accountNumber = @"example-number";
+    accountDetails.dateOfBirth = @"example-date";
+    accountDetails.postCode = @"example-post-code";
+
+configuration.primaryAccountDetails = accountDetails;
