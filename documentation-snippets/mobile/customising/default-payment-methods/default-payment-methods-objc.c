f1b02d64438f5a2b2e59906556247e7e2a9c3dd7
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 09:41:33 +0100

    Update default-payment-methods-objc.c

--- documentation-snippets/mobile/customising/default-payment-methods/default-payment-methods-objc.c
+++ documentation-snippets/mobile/customising/default-payment-methods/default-payment-methods-objc.c
@@ -1,1 +1,7 @@
-.
+//iOS
+//Set the paymentMethods property in the JPConfiguration object with your list of payment methods:
+
+configuration.paymentMethods = @[  
+    JPPaymentMethod.card,  
+    JPPaymentMethod.iDeal
+];
