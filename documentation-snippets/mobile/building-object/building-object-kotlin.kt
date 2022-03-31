c6323e3a4d01f6773b48faa2e8c432e37ccbe0e2
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 15:16:06 +0100

    Update building-object-kotlin.kt

--- documentation-snippets/mobile/building-object/building-object-kotlin.kt
+++ documentation-snippets/mobile/building-object/building-object-kotlin.kt
@@ -13,3 +13,23 @@ val amount = Amount.Builder()
 val reference = Reference.Builder()                 
     .setConsumerReference('my-consumer-reference')                 
     .build()
+
+//To set up the Judo configuration:
+//Set the following required parameters:
+//The token and secret values, OR Create the authorisation object:
+//Set the SDK to run in sandbox mode
+//Set the following objects:
+//- Judo ID
+//- Authorisation object (if using this method)
+//- Amount
+//- Reference
+
+//Create the authorisation object:
+val authorization = PaymentSessionAuthorization.Builder()
+       .setPaymentSession(paymentSession)
+       .setApiToken(token)
+       .build()
+
+//Set the authorisation object when invoking Judo builder:
+val builder = Judo.Builder(widgetType)
+            .setAuthorization(authorization)
