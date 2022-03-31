96712836a15ae2d2deefd6160c31c0ec0c2e526a
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:15:35 +0100

    Update supported-card-networks-kotlin.kt

--- documentation-snippets/mobile/customising/supported-card-networks/supported-card-networks-kotlin.kt
+++ documentation-snippets/mobile/customising/supported-card-networks/supported-card-networks-kotlin.kt
@@ -1,1 +1,11 @@
-.
+//Android
+//Set the setSupportedCardNetworks on the Judo builder with an array of CardNetwork values.
+
+//For the purpose of this exercise, Visa and MasterCard are selected as the supported card networks:
+
+val networks = arrayOf(CardNetwork.VISA, CardNetwork.MASTERCARD)
+
+val judo = Judo.Builder(PaymentWidgetType.CARD_PAYMENT)
+            ...
+            .setSupportedCardNetworks(networks)
+            .build()
