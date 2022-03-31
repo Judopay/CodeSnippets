030b6716e79ad91c52c062b328681e3975f09d10
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-03-31 12:12:25 +0100

    Update primary-acc-details-kotlin.kt

--- documentation-snippets/mobile/customising/primary-acc-details/primary-acc-details-kotlin.kt
+++ documentation-snippets/mobile/customising/primary-acc-details/primary-acc-details-kotlin.kt
@@ -1,1 +1,14 @@
-.
+//Android
+//Call the setPrimaryAccountDetails on the Judo builder:
+
+val primaryAccountDetails = PrimaryAccountDetails.Builder()
+                              .setName("example-name")
+                              .setAccountNumber("example-account-number")
+                              .setDataOfBirth("example-date")
+                              .setPostCode("example-postcode")
+                              .build()
+
+val judo = Judo.Builder(PaymentWidgetType.CARD_PAYMENT)
+            ...
+            .setPrimaryAccountDetails(primaryAccountDetails)
+            .build()
