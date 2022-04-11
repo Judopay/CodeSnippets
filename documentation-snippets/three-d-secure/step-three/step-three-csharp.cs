a813830d95d6f5d6708c70b356ac8f8a9b6cfa2b
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-11 16:18:56 +0100

    Update step-three-csharp.cs

--- documentation-snippets/three-d-secure/step-three/step-three-csharp.cs
+++ documentation-snippets/three-d-secure/step-three/step-three-csharp.cs
@@ -1,9 +1,18 @@
-// (1) Resume the transaction flow to Judopay: 
+// (1) Once the additional device data has been collected, create an instance of the ResumeThreeDSecureTwo Model:
+
+var resumeModel = new ResumeThreeDSecureTwoModel()
+{
+    CV2 = "452",
+    MethodCompletion = MethodCompletion.Yes
+};
+
+
+// (2) Resume the transaction flow to Judopay: 
 //Use the ReceiptId from the original response
 
 var resumeResult = await client.ThreeDs.Resume3DSecureTwo(result.Response.ReceiptId, resumeModel);
 
-//Check the response
+// (3) Check the response
 
 //If no additional transaction checks are required, you will receive the usual paymentReceipt response.
 //If additional transaction checks are required, you will receive the completion response
