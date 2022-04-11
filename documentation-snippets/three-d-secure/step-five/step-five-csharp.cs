26e5bc8ebaeaff0b069d0b76ec32dd418ac33af7
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-11 14:43:31 +0100

    Update step-five-csharp.cs

--- documentation-snippets/three-d-secure/step-five/step-five-csharp.cs
+++ documentation-snippets/three-d-secure/step-five/step-five-csharp.cs
@@ -1,4 +1,11 @@
-// (1) Complete the transaction flow to Judopay: 
+// (1) Create an instance of the CompleteThreeDSecureTwo Model:
+
+var completeModel = new CompleteThreeDSecureTwoModel()
+{
+    CV2 = "452"
+};
+
+// (2) Complete the transaction flow to Judopay: 
 
 var completeResult = await client.ThreeDs.Complete3DSecureTwo(result.Response.ReceiptId, completeModel);
 
