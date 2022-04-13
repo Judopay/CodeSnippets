b2054309d7870a596da8dcd0be520540a51e9cd1
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-13 15:02:25 +0100

    Update step-five-php.php

--- documentation-snippets/three-d-secure/step-five/step-five-php.php
+++ documentation-snippets/three-d-secure/step-five/step-five-php.php
@@ -1,8 +1,3 @@
-// (1) Check the response.
-//If no additional transaction checks are required, you will receive the usual paymentReceipt response.
-//If additional transaction checks are required, you will receive the completion response.
-
-// (2) Complete the transaction flow to Judopay.
 CompleteThreeDSecureTwo:
    $completeRequest = $judopay3ds2->getModel('CompleteThreeDSecureTwo');
         $attributes = [
