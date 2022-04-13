eb13e8eb2d451525679300536219714c1c451149
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-13 14:16:55 +0100

    Update step-one-php.php

--- documentation-snippets/three-d-secure/step-one/step-one-php.php
+++ documentation-snippets/three-d-secure/step-one/step-one-php.php
@@ -1,3 +1,4 @@
+// (1) Prepare the 3dsecure2 payment:
 $payment = $judopay->getModel('Payment');    
    $attributes = [
         'amount' => 12.99,
@@ -18,3 +19,26 @@ $payment = $judopay->getModel('Payment');
         ]
     ]  
     ];   $payment->setAttributeValues($attributes);    
+
+// (2) Send the 3dsecure2 payment:
+try
+    {
+        $cardPayment->setAttributeValues($attributes);
+        $response = $cardPayment->create();
+    } catch (\Exception $e) {
+        return;
+    }
+
+// (3) Check the response:
+//Challenge response example requesting additional device data is needed for 3D Secure 2
+{
+  "Response": {
+    "ThreeDSecure": {
+      "methodUrl": "https://example.com/pay-sim/sim/acs",
+      "version": "2.1.0",
+      "md": "ewogICJ0aHJlZURTU2VydmVyVHJhbnNJRCIgOiAiYjNjY2IxYWItZTk5"
+    },
+    "receiptId": "68869013641206075392",
+    "message": "Issuer ACS has requested additional device data gathering",
+    "result": "Additional device data is needed for 3D Secure 2"
+  },
