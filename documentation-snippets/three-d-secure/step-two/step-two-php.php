1df6a017cb199d5f645df189e12e0188c5a858a5
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-13 14:21:40 +0100

    Delete step-two-php.php

--- documentation-snippets/three-d-secure/step-two/step-two-php.php
+++
@@ -1,9 +0,0 @@
-// (1) Send the 3D Secure 2 payment:
-
- try
-    {
-        $cardPayment->setAttributeValues($attributes);
-        $response = $cardPayment->create();
-    } catch (\Exception $e) {
-        return;
-    }
