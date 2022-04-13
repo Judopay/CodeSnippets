c544066f0fea7ddca64a57104cb94445a995edae
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-13 14:12:24 +0100

    Update step-one-csharp.cs

--- documentation-snippets/three-d-secure/step-one/step-one-csharp.cs
+++ documentation-snippets/three-d-secure/step-one/step-one-csharp.cs
@@ -22,3 +22,20 @@ var paymentModel = new CardPaymentModel()
         ScaExemption = ThreeDSecureTwoScaExemption.SecureCorporate
     }
 };
+
+// (2) Send the 3ds2 request to Judopay
+var result = await client.Payments.Create(paymentModel);
+
+
+// (3) Challenge response example requesting additional device data is needed for 3D Secure 2
+{
+  "Response": {
+    "ThreeDSecure": {
+      "methodUrl": "https://example.com/pay-sim/sim/acs",
+      "version": "2.1.0",
+      "md": "ewogICJ0aHJlZURTU2VydmVyVHJhbnNJRCIgOiAiYjNjY2IxYWItZTk5"
+    },
+    "receiptId": "68869013641206075392",
+    "message": "Issuer ACS has requested additional device data gathering",
+    "result": "Additional device data is needed for 3D Secure 2"
+  },
