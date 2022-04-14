749a2411e9402da9fcee03a3aa24b7eac7f80721
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-04-14 16:27:33 +0100

    Update add-form-javascript.js

--- documentation-snippets/web-sdk/add-form/add-form-javascript.js
+++ documentation-snippets/web-sdk/add-form/add-form-javascript.js
@@ -1,20 +1,23 @@
 <script>
 var configMinimum = {
+    isGeoLocationGatheringAllowed: true,
     iframe: {
     language: "en",
     errorFieldId: 'judopay-errors',
     showCardTypeIcons: true,
+    layout: "vertical",
     cardTypeIconRight: "10px",
     cardTypeIconTop: "-2px",
     backgroundColor: "#FFFFFF",
-    layout: "compact",
     enabledPaymentMethods: ['CARD'],
+    defaultCountryCode: 'UK',
     isCountryAndPostcodeVisible: false,
     isCardHolderNameVisible: true,
     errorsDisplay: "HIDE_UNDER_FIELDS",
     disableUnderline: false,
+    shouldAutofocus: true,
+            }
     }
-}
 </script>
 
 //Create the iframe in a script tag:
