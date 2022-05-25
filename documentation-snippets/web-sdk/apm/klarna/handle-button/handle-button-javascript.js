91836c5d5a190e6986e67ebb99c022d68c83c3a2
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-05-25 15:05:05 +0100

    Update handle-button-javascript.js

--- documentation-snippets/web-sdk/apm/klarna/handle-button/handle-button-javascript.js
+++ documentation-snippets/web-sdk/apm/klarna/handle-button/handle-button-javascript.js
@@ -1,46 +1,44 @@
 klarnaButton.onclick = async () => {
-   	const myPaymentToken = 'my-token-here'
-   	const products = [
-         	{
-                	name: 'name-here',
-                	quantity: 1,
-                	unit_price: 1.05,
-                	total_amount: 1.05
-         	}
-   	]
+       const myPaymentToken = 'my-token-here'
+       const products = [
+             {
+                    name: 'BatteryPowerPack',
+                    quantity: 1,
+                    unit_price: 1000,
+                    total_amount: 1001
+             }
+       ]
  
-   	const myConfiguration = {
-        	paymentMethod: 'Klarna',
-        	judoId: 'my-judo-id',
-        	currency: 'my-currency-code-here',
-        	amount: '1.05',
-        	country: 'my-shipping-address-country',
-        	accountHolderName: 'account-holder-name',
-        	merchantPaymentReference: 'my-payment-reference',
-        	merchantConsumerReference: 'your-consumer-reference',
-        	taxAmount: '0.05',
-        	mobileNumber: '0799999999',
-        	emailAddress: 'email@me.com',
-        	apmData: {
-            	billingAddress: {
-       	         address1: 'cardAddressAddress1',
-                	address2: 'cardAddressAddress2',
-                	city: 'cardAddressTown',
-                	country: 'shippingAddressCountry',
-                	postcode: 'cardAddressPostCode'
-     	       },
-            	firstName: 'klarnaFirstName',
-            	lastName: 'klarnaLastName',
-            	mobileNumber: '0799999999',
-            	products: JSON.stringify(products)
-        	}
-	}
+       const myConfiguration = {
+                       judoId: "{{judoId}}",
+                        currency: "GBP",
+                        amount: 10,
+                        country: "GB",
+                        accountHolderName: "Account Holder Name",
+                        merchantPaymentReference: "1",
+                        merchantConsumerReference: "1",
+                        taxAmount: 0.01,
+                        mobileNumber: "00441895808221",
+                        emailAddress: "john@doe.com",
+                    apmData: {
+                        firstName: "John",
+                        lastName: "Doe",
+                        mobileNumber: "00441895808221",
+                   billingAddress: {
+                        address1: "13 New Burlington St",
+                        address2: "Apt 214",
+                        city: "London",
+                        country: "GB",
+                        postcode: "W13 3BG"
+        }
+    },
+                    paymentMethod: "Klarna"
+}
  
- 
-   	try {
-         	const result = await judopay.invokePaymentWithKlarna(myPaymentToken, myConfiguration)
-         	console.log(result)
-   	} catch (myError) {
-   		console.error(myError)
-   	}
+       try {
+             const result = await judopay.invokePaymentWithKlarna(myPaymentToken, myConfiguration)
+             console.log(result)
+       } catch (myError) {
+           console.error(myError)
+       }
 }
