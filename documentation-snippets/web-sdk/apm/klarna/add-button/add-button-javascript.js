4a758c22c7ecf27d4fe0fb32f6f8fbbfabcb78dd
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-05-25 14:52:28 +0100

    Create add-button-javascript.js

---
+++ documentation-snippets/web-sdk/apm/klarna/add-button/add-button-javascript.js
@@ -0,0 +1,10 @@
+const klarnaButton = KlarnaButton({
+   	backgroundColor: '#ffb3c7',
+   	borderRadius: 4,
+   	color: '#171717',
+   	height: 36,
+   	width: 280,
+   	label: 'Pay now'
+})
+ 
+myContainer.append(klarnaButton)
