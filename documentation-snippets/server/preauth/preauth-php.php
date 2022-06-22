699788e072a35e25abd351cc2bd420c0e1d40be3
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-06-22 10:44:15 +0100

    Update preauth-php.php

--- documentation-snippets/server/preauth/preauth-php.php
+++ documentation-snippets/server/preauth/preauth-php.php
@@ -1,23 +1,23 @@
-//Prepare the preauth
-$preauth = $judopay->getModel('Preauth');    
-        $attributes = [
-        'amount' => 12.99,
-        'cardNumber' => '4976000000003436',
-        'expiryDate' => '12/25',
-        'yourConsumerReference' => 'yourConsumerReference',
-        'yourPaymentReference' => 'yourPaymentReference',
-        'cv2' => '452',
-        'currency' => 'GBP',
-        'judoId' => 'yourJudoId'
-    ];  $preauth->setAttributeValues($attributes);
-    
-    //Send the preauth
-    try {
-        $response = $preauth->create();
-        // Handle successful payment
-        return;
-    } catch (\Exception $e) {
-       echo $e->getMessage();
-        // Handle the errors
-        return;
-    }
+// Create an instance of the Preauth model
+$preauth = $judopay->getModel('Preauth');
+
+// Prepare the attributes
+$attributes = [
+    'judoId' => 'yourJudoId',
+    'cardNumber' => '4976000000003436',
+    'expiryDate' => '12/25',
+    'yourConsumerReference' => 'yourConsumerReference'
+    'yourPaymentReference' => 'yourPaymentReference'
+    'cv2' => '452',
+    'amount' => 1.01,
+    'currency' => 'GBP',
+];
+
+try {
+    // Set attributes and send the request to Judopay
+    $preauth->setAttributeValues($attributes);
+    $response = $preauth->create();
+} 
+catch (\Exception $e) {
+    return $e;
+}
