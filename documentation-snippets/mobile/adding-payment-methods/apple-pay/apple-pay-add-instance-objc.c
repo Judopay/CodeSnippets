3aa87f958ef97fc9639ada4b78c1435a03f6b68e
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-06-28 17:36:30 +0100

    Create apple-pay-add-instance-objc.c

---
+++ documentation-snippets/mobile/adding-payment-methods/apple-pay/apple-pay-add-instance-objc.c
@@ -0,0 +1,5 @@
+[judo invokeApplePayWithMode:TransactionModePayment               
+	configuration:myApplePayConfiguration                  
+	completion:^(JPResponse *response, NSError *error) {    
+	// Handle response or error
+}];
