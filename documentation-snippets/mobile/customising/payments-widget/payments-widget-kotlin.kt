eee44d3b598dd013e942ee7ccb63cbfdfc7c01c6
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-06 16:30:33 +0100

    Update payments-widget-kotlin.kt

--- documentation-snippets/mobile/customising/payments-widget/payments-widget-kotlin.kt
+++ documentation-snippets/mobile/customising/payments-widget/payments-widget-kotlin.kt
@@ -1,11 +1,8 @@
-//To set up the Payments Widget for Android
 //Change the PaymentWidgetType to either:
-
 //PAYMENT_METHODS or, 
-
 //PRE_AUTH_PAYMENT_METHODS
-
 //For example:
+
 val judo = Judo.Builder(PaymentWidgetType.PAYMENT_METHODS)
             .setApiToken("my-token")
             .setApiSecret("my-secret")
@@ -17,6 +14,7 @@ val judo = Judo.Builder(PaymentWidgetType.PAYMENT_METHODS)
             .build()
             
 //Start the Judo Activity:
+
 val intent = Intent(this, JudoActivity::class.java);
 intent.putExtra(Judo.JUDO_OPTIONS, judo);
 startActivityForResult(intent, PAYMENT_REQUEST_CODE);
