64dfdea160151cefc7f71fdb4aadc8d17139795f
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-06 16:28:19 +0100

    Update add-scan-card-btn-kotlin.kt

--- documentation-snippets/mobile/customising/add-scan-card-btn/add-scan-card-btn-kotlin.kt
+++ documentation-snippets/mobile/customising/add-scan-card-btn/add-scan-card-btn-kotlin.kt
@@ -1,11 +1,11 @@
-//Android
 //To add the Scan Card library:
 //Navigate to your build.gradle file
-
 //In the dependencies section add:
+
 'cards.pay:paycardsrecognizer:1.1.0'
 
 //An example of how your build.gradle might look:
+
 android {
     ...
 }
