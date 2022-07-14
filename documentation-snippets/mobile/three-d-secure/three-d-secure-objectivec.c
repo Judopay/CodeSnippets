d8f993b1cc5b0461f437d50b5d2dd008eef316b5
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-14 13:56:20 +0100

    Create three-d-secure-objectivec.c

---
+++ documentation-snippets/mobile/three-d-secure/three-d-secure-objectivec.c
@@ -0,0 +1,54 @@
+JPAmount *amount = [[JPAmount alloc] initWithAmount:@"1.50" currency:@"GBP"];
+JPReference *reference = [[JPReference alloc] initWithConsumerReference:@"consumer-reference" paymentReference:@"payment-reference"];
+
+JPUIConfiguration *uiConfiguration = [JPUIConfiguration new];
+
+// sets whether 3DS 2.0 UI billing information screen should be presented to the user
+uiConfiguration.shouldAskForBillingInformation = YES;
+
+// in case you don't want to present billing info screen to the user, you can set the address instead
+JPAddress *address = [[JPAddress alloc] initWithAddress1:@"My house"
+                                                address2:@"My street"
+                                                address3:@"My area"
+                                                    town:@"My town"
+                                          billingCountry:@"826"
+                                                postCode:@"TR14 8PA"
+                                             countryCode:@826];
+
+JPConfiguration *configuration = [[JPConfiguration alloc] initWithJudoID:@"my-judo-id" amount:amount reference:reference];
+configuration.uiConfiguration = uiConfiguration;
+
+configuration.cardAddress = address;
+
+
+// sets the value for challenge request indicator,
+// possible values:
+//
+// noPreference
+// noChallenge
+// challengePreferred
+// challengeAsMandate
+//
+configuration.challengeRequestIndicator = @"challengeAsMandate";
+
+// sets the value for SCA exemption,
+// possible values:
+//
+// lowValue
+// secureCorporate
+// trustedBeneficiary
+// transactionRiskAnalysis
+//
+configuration.scaExemption = @"lowValue";
+
+// email address
+configuration.emailAddress = @"email@me.com";
+
+// sets the maximum timeout for 3DS 2.0 transactions in minutes
+configuration.threeDSTwoMaxTimeout = 30;
+
+// sets phone number country code
+configuration.phoneCountryCode = @"44";
+
+// phone number
+configuration.mobileNumber = @"11223344556677";
