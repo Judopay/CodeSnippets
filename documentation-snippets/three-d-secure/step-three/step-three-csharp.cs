633472d1f1d886c1e7c569ebb155cc34c70f3b68
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-19 17:53:34 +0100

    Update step-three-csharp.cs

--- documentation-snippets/three-d-secure/step-three/step-three-csharp.cs
+++ documentation-snippets/three-d-secure/step-three/step-three-csharp.cs
@@ -4,7 +4,13 @@ var resumeModel = new ResumeThreeDSecureTwoModel()
 {
     CV2 = "452",
     MethodCompletion = MethodCompletion.Yes
-};
+    "primaryAccountDetails":{
+      "name":"John Smith",
+      "accountNumber":"123456",
+      "dateOfBirth":"1980-01-01",
+      "postCode":"EC2A 4DP"
+      }        
+   };
 
 // (2) Resume the transaction flow to Judopay: 
 //Use the ReceiptId from the original response
