fe2f1a35242da76b5a1382fd2d091e4d1040afbb
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-19 17:55:26 +0100

    Update step-five-php.php

--- documentation-snippets/three-d-secure/step-five/step-five-php.php
+++ documentation-snippets/three-d-secure/step-five/step-five-php.php
@@ -2,7 +2,12 @@ CompleteThreeDSecureTwo:
    $completeRequest = $judopay3ds2->getModel('CompleteThreeDSecureTwo');
         $attributes = [
             'receiptId' => $response['receiptId'],
-            'cv2' => '452'
+            'cv2' => '452',
+            "primaryAccountDetails":{
+               "name":"John Smith",
+               "accountNumber":"123456",
+               "dateOfBirth":"1980-01-01",
+               "postCode":"EC2A 4DP"
         ];
         $completeRequest->setAttributeValues($attributes);       
  $response = $completeRequest->update();
