880bef354ee9f0cf1470973ea685b46bd4e8e2fd
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-19 17:52:05 +0100

    Update step-three-php.php

--- documentation-snippets/three-d-secure/step-three/step-three-php.php
+++ documentation-snippets/three-d-secure/step-three/step-three-php.php
@@ -6,7 +6,13 @@ ResumeThreeDSecureTwo:
         $attributes = [
             'receiptId' => $response['receiptId'],
             'cv2' => '452',
-            'methodCompletion' => 'no'
+            'methodCompletion' => 'no',
+            "primaryAccountDetails":{
+                "name":"John Smith",
+                "accountNumber":"123456",
+                "dateOfBirth":"1980-01-01",
+                "postCode":"EC2A 4DP"
+              },
         ];
         $resumeRequest->setAttributeValues($attributes);       
  $response = $resumeRequest->update();
