65130a9a506429f57542a6c627cbdedb9cd6a681
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-07-19 17:54:44 +0100

    Update step-five-csharp.cs

--- documentation-snippets/three-d-secure/step-five/step-five-csharp.cs
+++ documentation-snippets/three-d-secure/step-five/step-five-csharp.cs
@@ -1,7 +1,13 @@
 // (1) Create an instance of the CompleteThreeDSecureTwo Model:
 var completeModel = new CompleteThreeDSecureTwoModel()
 {
-    CV2 = "452"
+    CV2 = "452",
+    "primaryAccountDetails":{
+      "name":"John Smith",
+      "accountNumber":"123456",
+      "dateOfBirth":"1980-01-01",
+      "postCode":"EC2A 4DP"
+   }
 };
 
 // (2) Complete the transaction flow to Judopay: 
