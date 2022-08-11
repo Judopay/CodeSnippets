d0bd26bcf9c02dd392032a80fb3afa36ed0eb451
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-08-11 11:25:10 +0100

    Update react-javascript.js

--- documentation-snippets/mobile/react/react-javascript.js
+++ documentation-snippets/mobile/react/react-javascript.js
@@ -1,5 +1,3 @@
-
-//An example of a package.json file with the Judopay SDK dependency:
 {
   "name": "my-app",
   "version": "1.0.0",
@@ -7,22 +5,10 @@
    ...
   },
   "dependencies": {
-   "judo-react-native": "^2.1.0",
+   "judokit-react-native": "3.4.0",
    ...
        },
   "devDependencies": {
     ...
   }
 }
-
-//To run the Judopay React Native SDK
-//Import the JudoPay class from the judo-react-native package
-
-  import JudoPay from 'judo-react-native'
-
-//Provide your token and secret
-  const judoPay = new JudoPay('my-token', 'my-secret')
-
-//Set the SDK in sandbox mode
-
-  judoPay.isSandboxed = true
