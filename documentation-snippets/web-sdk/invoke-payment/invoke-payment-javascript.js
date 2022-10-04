98919362f51b972d4dcc2a74aef9272c207f5604
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-10-04 13:56:18 +0100

    Update invoke-payment-javascript.js

--- documentation-snippets/web-sdk/invoke-payment/invoke-payment-javascript.js
+++ documentation-snippets/web-sdk/invoke-payment/invoke-payment-javascript.js
@@ -1,6 +1,5 @@
-const judopay = new JudoPay()
-   // Invoke payment method:
-        judopay
-        .invokePayment(paymentSession, paymentConfiguration)
-        .then(handleSuccess)
-        .catch(handleError)
+function handlePaymentButtonClick() {
+    judo.invokePayment(paymentSession, paymentConfiguration)
+    .then(handleSuccess)
+    .catch(handleError)
+}
