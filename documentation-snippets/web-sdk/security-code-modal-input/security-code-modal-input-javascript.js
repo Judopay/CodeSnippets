4357c86b787d2f1fd6acc4a44325a595d3c41974
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-10-04 17:05:07 +0100

    Create security-code-modal-input-javascript.js

---
+++ documentation-snippets/web-sdk/security-code-modal-input/security-code-modal-input-javascript.js
@@ -0,0 +1,30 @@
+//Here you can customise the Security Code (CVV/CV2) modal styles
+const securityCodeModalStyle = {
+    labelTextContent: 'Security code',
+    inputPlaceholder: 'Type your code here',
+    buttonTextContent: 'Submit',
+    style: {
+        form: {
+            padding: '20px 16px',
+            maxWidth: '300px',
+            margin: 'auto',
+            width: '80%',
+            borderRadius: '4px'
+        },
+        field: {
+            width: '100%',
+            padding: '8px',
+            color: '#999999'
+        },
+        label: {
+            width: '100%',
+            padding: '8px 0px 8px 0px',
+            color: '#999999'
+        },
+        button: {
+            backgroundColor: '#222',
+            borderRadius: '4px',
+            borderStyle: 'none',
+        }
+    }
+}
