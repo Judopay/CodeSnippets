77825bb9264ff5ea9e885ed0d549ca4ed4a43e1e
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-10-04 10:39:19 +0100

    Update add-form-javascript.js

--- documentation-snippets/web-sdk/add-form/add-form-javascript.js
+++ documentation-snippets/web-sdk/add-form/add-form-javascript.js
@@ -1,26 +1,22 @@
 //Script to create a minimum iframe:
 <script>
-var configMinimum = {
-    isGeoLocationGatheringAllowed: true,
-    iframe: {
-    language: "en",
-    errorFieldId: 'judopay-errors',
-    showCardTypeIcons: true,
-    layout: "vertical",
-    cardTypeIconRight: "10px",
+const iFrameConfiguration = {
+    isGeoLocationGatheringAllowed: true, 
+    iFrame: {
+    language: "en", 
+    errorFieldId: 'judopay-errors', 
+    showCardTypeIcons: true, 
+    layout: "vertical", 
+    cardTypeIconRight: "10px", 
     cardTypeIconTop: "-2px",
-    backgroundColor: "#FFFFFF",
-    enabledPaymentMethods: ['CARD'],
+    backgroundColor: "#FFFFFF", 
+    enabledPaymentMethods: ['CARD'], 
     defaultCountryCode: 'UK',
-    isCountryAndPostcodeVisible: false,
-    isCardHolderNameVisible: true,
-    errorsDisplay: "HIDE_UNDER_FIELDS",
-    disableUnderline: false,
-    shouldAutofocus: true,
+    isCountryAndPostcodeVisible: false, 
+    isCardHolderNameVisible: true, 
+    errorsDisplay: "HIDE_UNDER_FIELDS", 
+    disableUnderline: false, 
+    shouldAutofocus: true, 
             }
     }
 </script>
-
-//Create the iframe in a script tag:
-var judo = new JudoPay("yourAPIToken", true);
-var payment = judo.createCardDetails('payment-iframe',configMinimum);
