1a174bce301febe9732513548e83f606b723ae4f
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-10-06 11:10:00 +0100

    Update add-button-javascript.js

--- documentation-snippets/web-sdk/apm/paypal/add-button/add-button-javascript.js
+++ documentation-snippets/web-sdk/apm/paypal/add-button/add-button-javascript.js
@@ -1,5 +1,51 @@
-judo.getPayPalButton(configuration)  
-.then(button => {   
-button.render('#paypal-button-container');  
-}) 
-.catch(error => console.error(error));
+<body>
+
+  <div id="#paypal-button-container" ></div>
+  
+<script>
+  
+  const payPalConfiguration = {
+      style: {color: 'blue', height: 34},
+      merchantId: 'yourPaypalMerchantId',
+      amount: '1.01',
+      currency: 'GBP',
+      merchantPaymentReference: 'yourPaymentReference',
+      judoId: 'yourJudoId',
+      dynamicDescriptor: 'dynamicDescriptor',
+      paymentSession: 'yourPaymentSession',
+      consumer: {
+          merchantConsumerReference: 'yourConsumerReference',
+          firstName: 'John',
+          surname: 'Doe',
+          emailAddress: 'example@domain.com'
+      },
+      billingAddress: {
+          address1: 'My House',
+          address2: 'My Street',
+          address3: '',
+          town: 'My Town',
+          city: 'My City',
+          country: 'GB',
+          postcode: 'TR14 8PA',
+      },
+      shippingAddress: {
+          address1: 'My House',
+          address2: 'My Street',
+          address3: '',
+          town: 'My Town',
+          city: 'My City',
+          country: 'GB',
+          postcode: 'TR14 8PA',
+      },
+      onSuccess: handleSuccess,
+      onError: handleError
+  }
+  
+  judo.getPayPalButton(payPalConfiguration) 
+      .then(button => {
+          button.render(document.getElementById("#paypal-button-container"));    
+          })
+      .catch(error => handleError(error))
+      
+</script>
+</body>
