3d8bebd0fbe04746f9a7238c3bb2d93dc380ecfc
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2022-10-06 12:15:11 +0100

    Update add-button-javascript.js

--- documentation-snippets/web-sdk/apm/klarna/add-button/add-button-javascript.js
+++ documentation-snippets/web-sdk/apm/klarna/add-button/add-button-javascript.js
@@ -1,10 +1,20 @@
-const klarnaButton = KlarnaButton({
-   	backgroundColor: '#ffb3c7',
-   	borderRadius: 4,
-   	color: '#171717',
-   	height: 36,
-   	width: 280,
-   	label: 'Pay now'
-})
- 
-myContainer.append(klarnaButton)
+<body>
+
+  <div id="klarna-button-container" ></div>
+  
+<script>
+  
+  const klarnaButton = judo.getKlarnaButton({
+    	backgroundColor: '#ffb3c7',
+    	borderRadius: 4,
+    	color: '#171717',
+    	height: 36,
+    	width: 280,
+    	label: 'Pay now'
+  })
+  
+  const container = document.getElementById("klarna-button-container")
+  container.append(klarnaButton)
+      
+</script>
+</body>
