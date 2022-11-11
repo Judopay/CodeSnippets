2729adba9a5388ded0ba35d7f34320af7f52d92a
Stuart Baillie <stuart.baillie@judopay.com>
2022-11-11 17:26:39 +0000

    JR-6556 : Update server code snippets for CheckCard

--- documentation-snippets/server/check-card/check-card-csharp.cs
+++ documentation-snippets/server/check-card/check-card-csharp.cs
@@ -1,13 +1,40 @@
 //Create an instance of the CheckCard Model
 var checkCardRequest = new CheckCardModel
-   {
-     CV2 = "452",
-     CardNumber = "4976000000003465",
-     ExpiryDate = "12/25",
-     JudoId = "yourJudoId", 
-     YourConsumerReference =  "yourConsumerReference",
-   };
+{
+    JudoId = "yourJudoId",
+    YourConsumerReference = "yourConsumerReference",
+    YourPaymentReference = "yourPaymentReference",
+    CardNumber = "4976000000003436",
+    ExpiryDate = "12/25",
+    CV2 = "452",
+    CardAddress = new CardAddressModel
+    {
+        Address1 = "41 Luke St",
+        PostCode = "EC2A 4DP",
+        Town = "London",
+        CountryCode = 826
+    },
+    // PrimaryAccountDetails only required for MCC6012 merchants
+    PrimaryAccountDetails = new PrimaryAccountDetailsModel
+    {
+        Name = "Smith",
+        AccountNumber = "1234567",
+        DateOfBirth = "2000-12-31",
+        PostCode = "EC2A 4DP"
+    },
+    // Following are for 3DS2 transactions
+    PhoneCountryCode = "44",
+    MobileNumber = "7999123456",
+    EmailAddress = "test.user@judopay.com",
+    CardHolderName = "John Doe",
+    ThreeDSecure = new ThreeDSecureTwoModel
+    {
+        AuthenticationSource = ThreeDSecureTwoAuthenticationSource.Browser,
+        ChallengeRequestIndicator = ThreeDSecureTwoChallengeRequestIndicator.ChallengeAsMandate,
+        MethodNotificationUrl = "https://yourMethodNotificationUrl",
+        ChallengeNotificationUrl = "https://yourChallengeNotificationUrl"
+    }
+};
 
 //Send the request to Judopay
-var result = await 
-client.CheckCards.Create(checkCardRequest)
+var response = await client.CheckCards.Create(checkCardRequest)
--- documentation-snippets/server/check-card/check-card-php.php
+++ documentation-snippets/server/check-card/check-card-php.php
@@ -1,22 +1,39 @@
 //Prepare the CheckCard request
-$checkCard = $judopay->getModel('CheckCard');    $attributes = [
+$checkCardRequest = $judopay->getModel('CheckCard');
+
+$checkCardRequest->setAttributeValues(
+    array(
+        'judoId' => 'yourJudoId',
+        'yourConsumerReference' => 'yourConsumerReference',
+        'yourPaymentReference' => 'yourPaymentReference',
         'cardNumber' => '4976000000003465',
         'expiryDate' => '12/25',
         'cv2' => '452',
-        'judoId' => 'yourJudoId',
-        'yourConsumerReference' => 'yourConsumerReference',
-        'yourPaymentReference' => 'yourPaymentReference' 
-    ];    $checkCard->setAttributeValues($attributes);
+        'cardAddress' => array(
+            'address1' => '41 Luke St',
+            'postCode' => 'EC2A 4DP',
+            'town' => 'London',
+            'countryCode' => 826
+        ),
+        // PrimaryAccountDetails only required for MCC6012 merchants
+        'primaryAccountDetails' => array(
+            'name' => 'Smith',
+            'accountNumber' => '1234567',
+            'dateOfBirth' => '2000-12-31',
+            'postCode' => 'EC2A 4DP'
+        ),
+        // Following are for 3DS2 transactions
+        'phoneCountryCode' => '44',
+        'mobileNumber' => '7999123456',
+        'emailAddress' => 'test.user@judopay.com',
+        'cardHolderName' => 'John Doe',
+        'threeDSecure' => array(
+            'authenticationSource'      => 'Browser',
+            'challengeRequestIndicator' => 'ChallengeAsMandate',
+            'methodNotificationUrl'     => 'https://yourMethodNotificationUrl',
+            'challengeNotificationUrl'  => 'https://yourMethodNotificationUrl'
+    )
+)
 
 //Send the request to Judopay
-try {
-    $response = $checkCard->create();
-    // Handle successful payment    
-     $cardToken = $response['cardDetails']['cardToken'];
-     } catch (\Exception $e) {
-    // Handle the errors
-    return;
-}
-
-//Get the card token from the response
-$cardToken = $response['cardDetails']['cardToken']; 
+$response = $checkCardRequest->create();
