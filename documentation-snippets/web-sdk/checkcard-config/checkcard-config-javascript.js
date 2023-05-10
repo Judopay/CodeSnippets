757e6931db80f823490d21e3e94377d5bb3280bc
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2023-05-10 19:29:50 +0100

    Update and rename checkcard-config-json.json to checkcard-config-javascript.js

---
+++ documentation-snippets/web-sdk/checkcard-config/checkcard-config-javascript.js
@@ -0,0 +1,18 @@
+const checkCardConfiguration = {
+    judoId: "yourJudoId",
+    phoneCountryCode: "44",
+    challengeRequestIndicator: "challengeAsMandate",
+    scaExemption: "SecureCorporate",
+    initialRecurringPayment: "false",
+    yourConsumerReference: "yourConsumerReference",
+    yourPaymentReference: "yourPaymentReference",
+    billingAddress: {
+        address1: "My house",
+        address2: "My street",
+        town: "My town",
+        postcode: "TR14 8PA",
+        country: 826
+    },
+    mobileNumber: "07999999999",
+    emailAddress: "contact@judopay.com",
+}
