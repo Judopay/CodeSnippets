afe6121054b84a6ddf139c0a3fab5af69b6974e8
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2023-05-10 19:34:14 +0100

    Update token-payment-javascript.js

--- documentation-snippets/web-sdk/token-payment/token-payment-javascript.js
+++ documentation-snippets/web-sdk/token-payment/token-payment-javascript.js
@@ -1,27 +1,27 @@
-    const tokenConfiguration = {
-        judoId: "youJudoId",
-        amount: 12.99,
-        currency: "GBP",
-        phoneCountryCode: "44",
-        mobileNumber: "07999999999",
-        emailAddress: "contact@judopay.com",
-        challengeRequestIndicator: "challengeAsMandate",
-        scaExemption: "lowValue",
-        initialRecurringPayment: false,
-        yourConsumerReference: "yourConsumerReference",
-        yourPaymentReference: "yourPaymentReference",
-        billingAddress: {
-                  address1: "My House",
-                  address2: "My Street",
-                  town: "My Town",
-                  country: "GB",
-                  postcode: "TR14 8PA",
-                  country: 826,
-                  state: "FL"
-        },   
+const tokenConfiguration = {
+     judoId: "youJudoId",
+     amount: 12.99,
+     currency: "GBP",
+     phoneCountryCode: "44",
+     mobileNumber: "07999999999",
+     emailAddress: "contact@judopay.com",
+     challengeRequestIndicator: "challengeAsMandate",
+     scaExemption: "lowValue",
+     initialRecurringPayment: false,
+     yourConsumerReference: "yourConsumerReference",
+     yourPaymentReference: "yourPaymentReference",
+     billingAddress: {
+             address1: "My House",
+             address2: "My Street",
+             town: "My Town",
+             country: "GB",
+             postcode: "TR14 8PA",
+             country: 826,
+             state: "FL"
+     },   
 
-      // Token specific params (above are the same as the paymentConfiguration)
-      cardToken: "yourCardToken", //mandatory
-      cardHolderName: "John Doe", //mandatory
-      shouldVerifySecurityCode: true
-    }
+   // Token specific params (above are the same as the paymentConfiguration)
+   cardToken: "yourCardToken", //mandatory
+   cardHolderName: "John Doe", //mandatory
+   shouldVerifySecurityCode: true
+}
