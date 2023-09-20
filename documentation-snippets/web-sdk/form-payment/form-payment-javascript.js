3050f976a7044f8f039756631fb7e06f743c3126
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2023-09-20 17:55:55 +0100

    Update form-payment-javascript.js

--- documentation-snippets/web-sdk/form-payment/form-payment-javascript.js
+++ documentation-snippets/web-sdk/form-payment/form-payment-javascript.js
@@ -59,7 +59,7 @@
                 address1: "My house",
                 address2: "My street",
                 town: "My town",
-                postcode: "TR14 8PA",
+                postCode: "TR14 8PA",
                 country: 826
             },
             mobileNumber: "07999999999",
