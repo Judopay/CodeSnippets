3862229168ca9f1d94c9c25a3c0e6a25aaea1ec8
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2023-09-20 17:54:26 +0100

    Update payment-javascript.js

--- documentation-snippets/web-sdk/payment/payment-javascript.js
+++ documentation-snippets/web-sdk/payment/payment-javascript.js
@@ -11,7 +11,7 @@ const paymentConfiguration = {
         address1: "My house",
         address2: "My street",
         town: "My town",
-        postcode: "TR14 8PA",
+        postCode: "TR14 8PA",
         country: 826
     },
     mobileNumber: "07999999999",
