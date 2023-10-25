1a1d91e0d91cc537cc51520a50b4aade78ca86b2
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2023-10-25 11:24:57 +0100

    Update ideal-config-javascript.js

--- documentation-snippets/web-sdk/apm/ideal/ideal-config/ideal-config-javascript.js
+++ documentation-snippets/web-sdk/apm/ideal/ideal-config/ideal-config-javascript.js
@@ -1,1 +1,10 @@
-
+const idealConfiguration = {
+    judoId: "yourJudoId",
+    merchantPaymentReference: "yourPaymentReference",
+    merchantConsumerReference: "yourConsumerReference",
+    currency: "EUR",
+    amount: 10,
+    country: "GB",
+    accountHolderName: "Account Holder Name",
+    paymentMethod: "IDEAL"
+}
