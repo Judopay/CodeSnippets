7768e49f96cf57cba86dd6b0cfd275babd7c89dc
AlisonTill <64002861+AlisonTill@users.noreply.github.com>
2024-02-02 11:40:25 +0000

    Update form-config-javascript.js

--- documentation-snippets/web-sdk/form-config/form-config-javascript.js
+++ documentation-snippets/web-sdk/form-config/form-config-javascript.js
@@ -1,20 +1,20 @@
-var exampleConfig = {
+const iFrameConfiguration = {
+    isGeoLocationGatheringAllowed: true,
     iframe: {
-        language: "en",
+        language: 'en',
         errorFieldId: 'judopay-errors',
-        cardTypeIconRight: "-400px",
-        cardTypeIconTop: "-50x",
-        backgroundColor: "#EAECEF",
+        cardTypeIconRight: '-400px',
+        cardTypeIconTop: '-50x',
+        backgroundColor: '#EAECEF',
         showCardTypeIcons: true,
-        layout: "vertical",
+        layout: 'vertical',
         enabledPaymentMethods: ['CARD'],
         defaultCountryCode: 'US',
         isCountryAndPostcodeVisible: false,
         isCardHolderNameVisible: false,
         disableUnderline: true,
-        errorsDisplay: "HIDE_UNDER_FIELDS",
-        disableUnderline: true,
-        shouldAutofocues: false,
+        errorsDisplay: 'HIDE_UNDER_FIELDS',
+        shouldAutofocus: false,
         fonts: [
             {
                 family: 'Roboto',
@@ -22,15 +22,14 @@ var exampleConfig = {
                 display: 'auto',
                 style: 'normal',
                 unicodeRange:
-                'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, 
-                 U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
+                  'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD',
                 weight: '400'
             }
         ],
         styles: {
             default: {
                 field: {
-                    fontFamily: "'Roboto', sans-serif",
+                    fontFamily: 'Roboto, sans-serif',
                     fontSize: '17px',
                     backgroundColor: '#e5e5e5',
                     borderTop: 'none',
@@ -59,57 +58,50 @@ var exampleConfig = {
                     backgroundColor: '#F0F0F0',
                     color: 'rgb(130, 130, 130)',
                     padding: '12px 6px 0',
-                    fontSize: '16px',
+                    fontSize: '16px'
                 },
                 label: {
-                    opacity: "1",
+                    opacity: '1',
                     color: 'rgb(130, 130, 130)'
                 }
             },
             error: {
                 field: {
                     backgroundColor: '#e5ecff',
-                    padding: '12px 6px 0',
+                    padding: '12px 6px 0'
                 },
                 label: {
-                    opacity: "1"
+                    opacity: '1'
                 }
             },
             valid: {
                 field: {
                     backgroundColor: '#e5ffec',
                     color: 'green',
-                    padding: '12px 6px 0',
+                    padding: '12px 6px 0'
                 },
                 label: {
                     color: 'green',
-                    opacity: "1"
+                    opacity: '1'
                 }
             },
             customLanguages: {
-                "en": {
+                en: {
                     fieldLabels: {
-                        cardNumberLabel: "Credit Card Number",
-                        expiryDateLabel: "Expiration Date",
-                        cardVerificationCodeLabel: "CVC"
+                        cardNumberLabel: 'Credit Card Number',
+                        expiryDateLabel: 'Expiration Date',
+                        cardVerificationCodeLabel: 'CVC'
                     },
                     fieldPlaceholders: {
-                        cardNumberPlaceholder: "xxxx xxxx xxxx xxxx",
-                        expiryDatePlaceholder: "MM / YY ",
-                        cardVerificationCodePlaceholder: "xxx"
+                        cardNumberPlaceholder: 'xxxx xxxx xxxx xxxx',
+                        expiryDatePlaceholder: 'MM / YY ',
+                        cardVerificationCodePlaceholder: 'xxx'
                     },
                     errors: {
-                        0: "Card number must be numeric",
-                        1: "Card number not valid",
-                        2: "Card type not recognised",
-                        3: "Card number required",
-                        4: "Card is expired",
-                        5: "Invalid expiry date",
-                        6: "Expiry date required",
-                        7: "{0} code too short",
-                        8: "{0} code too long",
-                        9: "{0} code must be numeric",
-                        10: "{0} code required"
+                        generic: 'Sorry, an error has occurred.',
+                        cardHolderNameRequired: 'Please enter a cardholder name',
+                        cardHolderNameTooShort: "Card Name can't be less than 4 characters",
+                        cardHolderNameSpecialChars: "Card Name can't contain numbers or special characters"
                     }
                 }
             }
